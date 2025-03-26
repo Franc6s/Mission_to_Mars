21fbba31b2e3102d1a32c0140ca50c966ea8994d
Francis Mangala <fdmangala@yahoo.com>
2025-03-26 14:35:44 -0400

    Update repository

--- scraping.py
+++ scraping.py
@@ -1,3 +1,28 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__, template_folder='templates')
+
+# Use flask_pymongo to set up Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"$set": mars_data}, upsert=True)
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
