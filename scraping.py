c4c27afa8e661f9693ba9710f5a246146837257e
Francis Mangala <fdmangala@yahoo.com>
2025-03-26 00:08:14 -0400

    Update Repository

--- scraping.py
+++ scraping.py
@@ -29,21 +29,21 @@ def scrape_all():
 
 
 def mars_news(browser):
-
+    try:
     # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
-    browser.visit(url)
+        url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+        browser.visit(url)
 
     # Optional delay for loading the page
-    browser.is_element_present_by_css('div.list_text', wait_time=1)
+        browser.is_element_present_by_css('div.list_text', wait_time=1)
 
     # Convert the browser html to a soup object and then quit the browser
-    html = browser.html
-    news_soup = soup(html, 'html.parser')
+        html = browser.html
+        news_soup = soup(html, 'html.parser')
 
     # Add try/except for error handling
-    try:
+    
         slide_elem = news_soup.select_one('div.list_text')
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
         news_title = slide_elem.find('div', class_='content_title').get_text()
@@ -57,29 +57,29 @@ def mars_news(browser):
 
 
 def featured_image(browser):
+    try:
     # Visit URL
-    url = 'https://spaceimages-mars.com'
-    browser.visit(url)
+        url = 'https://x.com/konstructivizm/status/1903452858156044659/photo/1'
+        browser.visit(url)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_tag('button')[1]
-    full_image_elem.click()
+        full_image_elem = browser.find_by_tag('button')[1]
+        full_image_elem.click()
 
     # Parse the resulting html with soup
-    html = browser.html
-    img_soup = soup(html, 'html.parser')
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
 
     # Add try/except for error handling
-    try:
+    
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+         # Use the base url to create an absolute url
+        img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
     except AttributeError:
         return None
-
-    # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
-
+ 
     return img_url
 
 def mars_facts():
@@ -104,7 +104,7 @@ def mars_facts():
              
     #add the new function that will scrape the hemisphere data
 def hemisphere_url(browser):
-        url = 'https://marshemispheres.com/'
+        url = 'https://science.nasa.gov/mars/facts/'
         browser.visit(url)
         #create the list 
         hemisphere_image_urls = [ ] 
